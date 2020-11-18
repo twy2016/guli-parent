cc429bf4f983f6032977027734e632cf3fc6fb25
twy2016 <whgp1994@163.com>
2020-11-18 23:15:57 +0800

    代码提交

--- common/common-utils/src/main/java/com/twy/commonutils/ResultCode.java
+++ common/common-utils/src/main/java/com/twy/commonutils/ResultCode.java
@@ -5,7 +5,7 @@ package com.twy.commonutils;
  * @date 2020/11/13 22:01
  */
 public class ResultCode {
-    public static Integer SUCCESS = 0;
+    public static Integer SUCCESS = 20000;
 
-    public static Integer ERROR = 1;
+    public static Integer ERROR = 20001;
 }
